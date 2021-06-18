3b73c42406dcd070a20e8695ce8c9028e14d5be3
Zm@ster <b.barakel@hotmail.fr>
2021-06-18 10:21:19 +0200

    c'est update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './componants/contact/contact.component';
+import { ContactsComponent } from './componants/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule
